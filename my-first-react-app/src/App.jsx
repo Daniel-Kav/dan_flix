a3012519f4eccad57295b9eaba85400f112f5f74
daniel-kav <dankavatha01@gmail.com>
2023-11-22 02:10:52 +0300

    fully functional

--- my-first-react-app/src/App.jsx
+++ my-first-react-app/src/App.jsx
@@ -16,6 +16,7 @@ const movie1 = {
 const App = () => {
 
   const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
 
   const SearchMovies = async (title) => {
     const response = await fetch(`${API_URL}&s=${title}`);
@@ -32,20 +33,21 @@ const App = () => {
       <div className="search">
         <input 
           placeholder="Search for movies"
-          value="Search for movies"
-          onChange={() => {}}
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
           />
         <img src={SearchIcon}
           alt="Search"
-          onClick={() => {}}/>
+          onClick={()=>SearchMovies(searchTerm)}
+        />
       </div>
 
       {
         movies?.length > 0 ?(
           <div className="container">
-            {movies.map((movie) => {
+            {movies.map((movie) => (
               <MovieCard movie={movie} />
-            })
+            ))
             }
           </div>
         ) : (
