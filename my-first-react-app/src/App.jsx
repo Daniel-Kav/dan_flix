1c53845d37dd0440f7b642d5819370eb4c267dca
daniel-kav <dankavatha01@gmail.com>
2023-11-21 09:10:00 +0300

    added a movie card component

--- my-first-react-app/src/App.jsx
+++ my-first-react-app/src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import { useEffect } from 'react';
 import './App.css';
 
 const API_URL = 'http://www.omdbapi.com/?i=tt3896198&apikey=5b14d31c'
 
 function App() {
   
-
+  const Search
   return (
     <div>
       <h1>hello world!!!</h1>
